26322a8da1594c3bf763585b446185881ee1734f
Sergei Pavlov <unplanetx@gmail.com>
2019-08-03 22:09:44 +1000

    Исправляет замечания по шапке

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,8 @@
 var navMain = document.querySelector('.site-nav');
 var navToggle = document.querySelector('.site-nav__toggle');
 
-// navMain.classList.remove('site-nav--nojs');
+navMain.classList.add('site-nav--closed');
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('site-nav--closed')) {
-    navMain.classList.remove('site-nav--closed');
-    navMain.classList.add('site-nav--opened');
-  } else {
-    navMain.classList.add('site-nav--closed');
-    navMain.classList.remove('site-nav--opened');
-  }
+  navMain.classList.toggle('site-nav--closed');
 });
