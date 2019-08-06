4ad6d5b6aa871802cea1b895d152c2a82b4c3da5
Sergei Pavlov <unplanetx@gmail.com>
2019-08-07 02:03:53 +1000

    Выполняет оптимизацию графики для шапки

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,14 @@
 var navMain = document.querySelector('.site-nav');
 var navToggle = document.querySelector('.site-nav__toggle');
+var navMenuBurger = document.querySelector('.site-nav__menu-burger');
+var navMenuCross = document.querySelector('.site-nav__menu-cross');
+
 
 navMain.classList.add('site-nav--closed');
+navMenuCross.classList.add('visually-hidden');
 
 navToggle.addEventListener('click', function() {
   navMain.classList.toggle('site-nav--closed');
+  navMenuBurger.classList.toggle('visually-hidden');
+  navMenuCross.classList.toggle('visually-hidden');
 });
