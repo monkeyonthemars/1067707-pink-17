7b3bf938754b8a492ac43d4f0b9f5be6d74cacd1
Sergei Pavlov <unplanetx@gmail.com>
2019-08-25 00:17:32 +1000

    исправляет замечания

--- source/js/script.js
+++ source/js/script.js
@@ -2,13 +2,15 @@ var navMain = document.querySelector('.site-nav');
 var navToggle = document.querySelector('.site-nav__toggle');
 var navMenuBurger = document.querySelector('.site-nav__menu-burger');
 var navMenuCross = document.querySelector('.site-nav__menu-cross');
-
+var header = document.querySelector('.site-header');
 
 navMain.classList.add('site-nav--closed');
+header.classList.add('site-header--closed');
 navMenuCross.classList.add('visually-hidden');
 
 navToggle.addEventListener('click', function() {
   navMain.classList.toggle('site-nav--closed');
+  header.classList.toggle('site-header--closed');
   navMenuBurger.classList.toggle('visually-hidden');
   navMenuCross.classList.toggle('visually-hidden');
 });
